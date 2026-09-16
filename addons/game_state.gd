0e0941f260ddb863b2217e011fb6a00618284b28
Eddie Char <emchar1@gmail.com>
2026-09-15 21:32:53 -0700

    Fix and use GameState map_2d and 3d functions.

--- addons/game_state.gd
+++ addons/game_state.gd
@@ -16,8 +16,8 @@ var sfx_volume: float = 1.0
 # FUNCTIONS
 
 func map_2d_to_3d(vector2: Vector2) -> Vector3:
-	return Vector3(vector2.x, 0.0, vector2.y)
+	return Vector3(vector2.x, vector2.y, 0.0)
 
 
 func map_3d_to_2d(vector3: Vector3) -> Vector2:
-	return Vector2(vector3.x, vector3.z)
+	return Vector2(vector3.x, vector3.y)
