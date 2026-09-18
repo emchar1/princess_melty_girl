4fc034366d1b093665b794678b82e35363b59e0f
Eddie Char <emchar1@gmail.com>
2026-09-17 21:31:24 -0700

    Replace mario sprite with animated sprite. Level balancing.

--- player/player.gd
+++ player/player.gd
@@ -8,7 +8,12 @@ signal dead
 const RUN_MULTIPLIER_MIN = 0.85
 const RUN_MULTIPLIER_MAX = 1.25
 
+enum MoveState {
+	IDLE, JUMP, RUN, SKID
+}
+
 @export var coyote_hang_time: float = 0.18
+@onready var anim_sprite = $AnimatedSprite3D
 
 var speed: float = 10
 var jump_speed: float = 25
@@ -18,9 +23,12 @@ var deceleration: float = 50.0
 
 var current_platform: Platform
 var coyote_time: Timer
+
 var is_running: bool = false
 var is_jumping: bool = false
 var is_falling: bool = false
+
+var move_state: MoveState = MoveState.IDLE
 
 
 # FUNCTIONS
@@ -34,6 +42,7 @@ func _physics_process(delta: float) -> void:
 	_check_running()
 	_move_player(delta)
 	_process_jumping()
+	_update_moves()
 	_get_current_platform()
 	move_and_slide()
 	
@@ -64,23 +73,13 @@ func _move_player(delta: float):
 	var move_dir := Input.get_axis("move_left", "move_right")
 	
 	if move_dir != 0:
-		if is_on_floor():
-			velocity.x = move_toward(
-				velocity.x,
-				move_dir * speed * run_multiplier,
-				acceleration * delta
-			)
-		else:
-			velocity.x = move_toward(
-				velocity.x,
-				move_dir * speed * run_multiplier,
-				acceleration * delta
-			)
-			
-			# Old way: instant change... felt wrong.
-			#velocity.x = move_dir * speed * run_multiplier
+		velocity.x = move_toward(
+			velocity.x,
+			move_dir * speed * run_multiplier,
+			acceleration * delta
+		)
 		
-		$Sprite3D.flip_h = move_dir < 0
+		anim_sprite.flip_h = move_dir < 0
 	else:
 		velocity.x = move_toward(
 			velocity.x,
@@ -99,7 +98,42 @@ func _process_jumping():
 	if jump_pressed and can_jump and not is_jumping:
 		velocity.y = jump_speed
 		is_jumping = true
+		_update_moves_helper(MoveState.JUMP)
+		
 		AudioManager.play(AudioData.AudioKey.JUMP)
+
+
+func _update_moves():
+	if is_on_floor():
+		if velocity.x == 0:
+			_update_moves_helper(MoveState.IDLE)
+		else:
+			if (velocity.x < 0 and not anim_sprite.flip_h) or \
+			(velocity.x > 0 and anim_sprite.flip_h):
+				_update_moves_helper(MoveState.SKID)
+			else:
+				_update_moves_helper(MoveState.RUN, run_multiplier)
+	else:
+		_update_moves_helper(MoveState.JUMP)
+
+
+func _update_moves_helper(state: MoveState, speed_scale: float = 1.0):
+	if move_state == state:
+		return
+	
+	move_state = state
+	
+	match state:
+		MoveState.IDLE:
+			anim_sprite.play("idle")
+		MoveState.JUMP:
+			anim_sprite.play("jump")
+		MoveState.RUN:
+			anim_sprite.play("run")
+		MoveState.SKID:
+			anim_sprite.play("skid")
+	
+	anim_sprite.speed_scale = speed_scale
 
 
 func _get_current_platform():
