b84a16569f9f659f1a8c10d459f1e22f67430f69
Eddie Char <emchar1@gmail.com>
2026-09-22 11:51:28 -0700

    Refactor

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -17,7 +17,7 @@ func _ready() -> void:
 	_reset_add_time_label()
 	
 	# Uncomment to hide mouse cursor. Requires Godot restart
-	#Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 
 func _physics_process(_delta: float) -> void:
